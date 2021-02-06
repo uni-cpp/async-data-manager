27957682dffbefbaf54d616a6685ea2f6d69e15b
Sergey Polyakov <white.irbys@gmail.com>
2021-02-06 22:06:07 +0200

    Fix build for Windiws

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,12 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// @file app/main.cpp
-/// @brief Redundant calculations of crc sums for data blocks in multiple threads.
+/// @brief Extra calculations of crc sums for data blocks in multiple threads.
 /// @author Sergey Polyakov <white.irbys@gmail.com>
 /// @date 08.2020
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <uni/application/Application.hpp>
 
-#include <sysexits.h>
-#include <cstdint>
 #include <uni/common/Thread.hpp>
 
 int
@@ -16,12 +14,12 @@ main( int /*argc*/, char** /*argv*/ )
 {
     uni::common::Thread::set_current_thread_name( "Application" );
 
-    int exit_code{ EX_OK };
+    int exit_code{ 0 };
 
     uni::application::Application app( "Config.json" );
     if( !app.run( ) )
     {
-        exit_code = EX_SOFTWARE;
+        exit_code = 1;
     }
 
     return exit_code;
